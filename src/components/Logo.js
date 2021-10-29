b71e4cc3141f0463ab58bdb3aabae051f723a662
JBermudezG <jebermudez587@gmail.com>
2021-10-29 16:22:52 -0500

    Logo added

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header/logo.scss';
+
+export default function Logo(){
+    return(
+        <>
+            <div>
+                <p className="bermudez">BERMUDEZ</p>
+                <p className="signs">&#60; / &#62;</p>
+                <p className="web">WEB</p>
+            </div>
+        </>
+    )
+}
