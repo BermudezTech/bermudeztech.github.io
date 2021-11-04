d5b96fc2062399cab228a725fa7662234d763f42
JBermudezG <jebermudez587@gmail.com>
2021-11-03 20:20:21 -0500

    Added description

--- src/components/Pages/Main.js
+++ src/components/Pages/Main.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../styles/Pages/Main.scss';
 import Slider from './Slider';
 
 export default function Main(){
+    let [typingText, setTypingText] = useState("");
+    let [phraseNumber, setPhraseNumber] = useState(0);
+    let [phrasePosition, setPhrasePosition] = useState(0);
+    let [tempLetter, setTempLetter] = useState("");
+    let phrases = ["Desarrollador FRONTEND", 
+                "Estudiante de ingeniería", 
+                "Desarrollador de software"];
+
+    let typePhrase = () => {
+        if(phrasePosition <= phrases[phraseNumber].length){
+            setPhrasePosition(phrasePosition + 1);
+            setTempLetter(phrases[phraseNumber][phrasePosition]);
+        }else{
+            if(phraseNumber < (phrases.length - 1)){
+                setPhraseNumber(phraseNumber + 1);
+            }else{
+                setPhraseNumber(0);
+            }
+            let changePhrase = () => {
+                setTypingText("");
+                setPhrasePosition(0);
+                setTempLetter("");
+            }
+            setTimeout(changePhrase, 2000);
+        }
+    }
+
+    useEffect(() => {
+        if(typingText !== undefined && tempLetter !== undefined)
+        setTypingText(`${typingText}${tempLetter}`);
+
+        let temporizador = setTimeout(typePhrase, 40);
+        return () => clearTimeout(temporizador);
+        // eslint-disable-next-line
+    }, [phrasePosition]);
+
     return(
         <>
             <div className="Main">
@@ -12,7 +48,7 @@ export default function Main(){
                             <h1>Hola. Mi nombre es:</h1>
                             <h2>JOSÉ EDUARDO BERMÚDEZ GARAVITO</h2>
                         </div>
-                        <p><span><b>Soy:</b> Desarrollador FRONTEND</span></p>
+                        <p><span><b>Soy:</b> {typingText}<b className="cursor">|</b></span></p>
                     </div>
                     <Slider/>
                 </div>
